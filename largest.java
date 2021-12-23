7c02b5ce8b6391292211b1c961835c3d336eb919
sankalp47 <95141346+sankalp47@users.noreply.github.com>
2021-12-23 20:53:16 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,22 @@
+package FirstHand_Dec_23;
+
+public class Largest_No {
+
+	public static void main(String[] args) {
+		   int num1 = 100;
+		   int num2 = 200;
+		   int num3 = 300;
+		   if(num1>num2) {
+			   System.out.print(num1);
+		   }
+		   else if(num2>num3) {
+			   System.out.print(num2);
+		   }
+		   else {
+			   System.out.print(num3);
+		   }
+		   
+
+	}
+
+}
